abe4686e5697e064166fe1e997cff8bc4d0952ec
nWaTaR <24wataru0420@gmail.com>
2021-12-19 09:33:03 +0900

    service

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -2,7 +2,7 @@ import * as express from 'express';
 import * as helmet from 'helmet';
 import * as cors from 'cors';
 import { DetectionService } from '../services/DetectionService';
-// import { TextService } from '../services/TextService';
+import { TextService } from '../services/TextService';
 const app = express();
 app.use(helmet());
 app.use(cors({ 
@@ -45,13 +45,13 @@ router.post('/watson-speech-to-text/detection/:keywords', upload.single("audio")
   res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
 
-  let dummy = {
-    speechToText: 'D_ウーン そう 最近 増え ました かね レーベル 作って 百 百 烈 埋没 米 とか ビブリオ 羽織 を 映像 で セッション 保つ ん です ！ シルバー 運用 ユーザー できる 書面 ちゃう 人生 変わる わけ でも あり 愛 別姓 D_ア ＦＦＶ ジャパン ｉｎ あげて ション 違う 人生 ずっと 家 屏風山 ミシン D_マアネ ツアー を 見て 町 歩いて も ある とはいえ 前提 られて 中国 飲泉 泣いて いる 分の プロレス と 言って くれて ｐ 中 延 年 長期 に 侵入 町 の 人間 で ある で あり 以前 は 便器 恋 する ん です よ ハンソン で 検知 編入 を 目指す ビア も 圧延 円 アップル 猫 対 バルサ ション タオ ちゃん です いや それ を する プロヴァンス わいせつ 呆れ ちゃう 人 には もの を 番組 が でしょう ストーブ ブーム 弁 ちょっと ポイント って ＩＤ 説明 しにくい 先 が 思い アンルイス 電話 路面 スポーン 付いて いる ',
-    search: [
-      { keyword: 'する', position: 190, characterString: '前は便器恋するんですよハ' },
-      { keyword: 'する', position: 239, characterString: 'いやそれをするプロヴァン' }
-    ]
-  }
+  // let dummy = {
+  //   speechToText: 'D_ウーン そう 最近 増え ました かね レーベル 作って 百 百 烈 埋没 米 とか ビブリオ 羽織 を 映像 で セッション 保つ ん です ！ シルバー 運用 ユーザー できる 書面 ちゃう 人生 変わる わけ でも あり 愛 別姓 D_ア ＦＦＶ ジャパン ｉｎ あげて ション 違う 人生 ずっと 家 屏風山 ミシン D_マアネ ツアー を 見て 町 歩いて も ある とはいえ 前提 られて 中国 飲泉 泣いて いる 分の プロレス と 言って くれて ｐ 中 延 年 長期 に 侵入 町 の 人間 で ある で あり 以前 は 便器 恋 する ん です よ ハンソン で 検知 編入 を 目指す ビア も 圧延 円 アップル 猫 対 バルサ ション タオ ちゃん です いや それ を する プロヴァンス わいせつ 呆れ ちゃう 人 には もの を 番組 が でしょう ストーブ ブーム 弁 ちょっと ポイント って ＩＤ 説明 しにくい 先 が 思い アンルイス 電話 路面 スポーン 付いて いる ',
+  //   search: [
+  //     { keyword: 'する', position: 190, characterString: '前は便器恋するんですよハ' },
+  //     { keyword: 'する', position: 239, characterString: 'いやそれをするプロヴァン' }
+  //   ]
+  // }
 
   // var alertmsg = function(){
   //   console.log("10秒経過");
@@ -60,10 +60,10 @@ router.post('/watson-speech-to-text/detection/:keywords', upload.single("audio")
 
   await new Promise(resolve => setTimeout(resolve, 15000)) // 5秒待つ
   
-  console.log(dummy);
+  // console.log(dummy);
 
 
-  res.status(200).send(dummy);
+  // res.status(200).send(dummy);
 
   // const service = new TextService();
   // service
@@ -72,11 +72,11 @@ router.post('/watson-speech-to-text/detection/:keywords', upload.single("audio")
   //   .then(result => res.status(200).send(dummy))
   //   .catch(reason => {console.log('reason: ', reason); next()});
 
-  // const service = new TextService();
-  // const result = await service.run(req);
-  // console.log('result: ', result);
+  const service = new TextService();
+  const result = await service.run(req);
+  console.log('result: ', result);
 
-  // res.status(200).send(result);
+  res.status(200).send(result);
 })
 
 // -------------------------------------------------
